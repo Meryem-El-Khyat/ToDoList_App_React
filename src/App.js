864cce7ef103042e1dfd583935e013da412d0d71
Meryem-El-Khyat <elkhyat.meryem1997@gmail.com>
2024-11-05 14:56:16 +0100

    first commit by soufiane

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navigateur from './components/navigateur';
+import Shop from './components/Shop';
+import './App.css';
+
+function App() {
+    return (
+        <Router basename="/To-Do-List-App">
+            <div>
+                <Navigateur />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/shop" element={<Shop />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
